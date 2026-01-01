9063ed33922823d2f03c5cebd0534a91af572085
polar <dlrhksdn0416@gmail.com>
2026-01-01 22:36:11 +0900

    tests

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,26 +1,8 @@
-require("@nomicfoundation/hardhat-toolbox"); // 이 줄이 없으면 다시 getSigners 에러 납니다!
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
 
-module.exports = {
-  defaultNetwork: "hardhat",
-  networks: {
-    hardhat: {},
-  },
-  solidity: {
-    version: "0.8.28",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
-    },
-  },
-  paths: {
-    sources: "./contracts",
-    tests: "./test",
-    cache: "./cache",
-    artifacts: "./artifacts",
-  },
-  mocha: {
-    timeout: 40000,
-  },
+const config: HardhatUserConfig = {
+  solidity: "0.8.28",
 };
+
+export default config;
---
+++ test/MyToken.ts
@@ -0,0 +1,52 @@
+import hre from "hardhat";
+import { expect } from "chai";
+import { MyToken } from "../typechain-types";
+import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
+
+const decimals = 18n;
+
+describe("My Token", () => {
+  let myTokenC: MyToken;
+  let signers: HardhatEthersSigner[];
+
+  beforeEach(async () => {
+    signers = await hre.ethers.getSigners();
+    myTokenC = await hre.ethers.deployContract("MyToken", [
+      "MyToken",
+      "MT",
+      18,
+      100,
+    ]);
+  });
+
+  describe("TransferFrom (approve & transferFrom)", () => {
+    it("signer1 moves signer0 token using approve & transferFrom and checks balances", async () => {
+      const signer0 = signers[0];
+      const signer1 = signers[1];
+
+      const amount = hre.ethers.parseUnits("10", decimals); // 10 MT
+
+      // 1) approve: signer0가 signer1에게 권한 부여
+      await expect(myTokenC.approve(signer1.address, amount))
+        .to.emit(myTokenC, "Approval")
+        .withArgs(signer0.address, signer1.address, amount);
+
+      await expect(
+        myTokenC
+          .connect(signer1)
+          .transferFrom(signer0.address, signer1.address, amount)
+      )
+        .to.emit(myTokenC, "Transfer")
+        .withArgs(signer0.address, signer1.address, amount);
+
+      const bal0 = await myTokenC.balanceOf(signer0.address);
+      const bal1 = await myTokenC.balanceOf(signer1.address);
+
+      expect(bal1).to.equal(amount);
+      expect(bal0).to.equal(hre.ethers.parseUnits("100", decimals) - amount);
+
+      const remain = await myTokenC.allowance(signer0.address, signer1.address);
+      expect(remain).to.equal(0n);
+    });
+  });
+});
